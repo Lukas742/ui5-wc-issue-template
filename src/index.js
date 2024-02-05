8d381684351b54637ed23c51f3bb060f07adb063
Lukas Harbarth <lukas.harbarth@sap.com>
2024-02-05 14:53:18 +0100

    init

---
+++ src/index.js
@@ -0,0 +1,5 @@
+
+// doesn't work for some reason
+// import '@ui5/webcomponents-fiori/dist/Assets.js';
+// import '@ui5/webcomponents-icons/dist/AllIcons.js';
+import '@ui5/webcomponents/dist/Button.js';
